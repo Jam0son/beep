d904044fc6efb4f87cfadd7347002532767ed9db
Jam0son <102773780+Jam0son@users.noreply.github.com>
2022-04-01 16:31:40 -0700

    Add files via upload

---
+++ Beep.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+#include <windows.h> 
+
+    int main(){
+          for( ; ; ) {
+      printf("This will beep forever.\n");
+      Beep(523,500);
+   }
+       return 0;
+    }
